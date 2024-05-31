5363ef74fb83b002b10f40b35d233d1463796199
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-31 17:11:42 +0200

    created dashboard sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div id="sidebar">
+    <div class="logo">
+        <img src="https://www.svgrepo.com/show/314838/dashboard.svg" alt="logo">
+        <span class="fs-4">Admin</span>
+    </div>
+    <div class="d-flex">
+        <ul>
+            <li>
+                <a href="{{route('admin.dashboard')}}">Dashboard</a>
+            </li>
+            <li>
+                <a href="{{route('admin.projects.index')}}">Posts</a>
+            </li>
+        </ul>
+    </div>
+
+</div>
